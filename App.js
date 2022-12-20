fad2adae4850208bc7e5829cdb61babc3f161538
hannah1306 <suhyun1306@gmail.com>
2022-12-20 15:41:44 +0900

    update app.js

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Tab = createMaterialTopTabNavigator();
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>뭐볼래?</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>어디서볼래?</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={{ alignItems:'center'}}>
+        <Text>광고</Text>
+      </View>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="뭐볼래?" component={HomeScreen} />
+          <Tab.Screen name="어디서볼래?" component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
