48d501a457fd3525ad7c225089a6bf64ace8c211
hannah1306 <suhyun1306@gmail.com>
2022-12-21 15:39:25 +0900

    update app.js

--- App.js
+++ App.js
@@ -1,25 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { ScrollView } from 'react-native-web';
 
+//상단 텝
 const Tab = createMaterialTopTabNavigator();
 
-function HomeScreen() {
+//뭐볼래?
+function WhatScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>뭐볼래?</Text>
-    </View>
+    <ScrollView>
+      <Button title="넷플릭스"></Button>
+      <Button title="웨이브"></Button>
+      <Button title="티빙"></Button>
+      <Button title="쿠팡플레이"></Button>
+      <Button title="시즌"></Button>
+      <Button title="디즈니플러스"></Button>
+      <Button title="왓챠"></Button>
+      <Button title="유튜브프리미엄"></Button>
+      <Button title="아마존프라임"></Button>
+      <Button title="애플TV"></Button>
+    </ScrollView>
   );
 }
 
-function SettingsScreen() {
+//어디서볼래?
+function WhereScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>어디서볼래?</Text>
     </View>
   );
 }
+
+const ottData = [
+  {
+
+  }
+]
 
 export default function App() {
   return (
@@ -29,8 +48,8 @@ export default function App() {
       </View>
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="뭐볼래?" component={HomeScreen} />
-          <Tab.Screen name="어디서볼래?" component={SettingsScreen} />
+          <Tab.Screen name="뭐볼래?" component={WhatScreen} />
+          <Tab.Screen name="어디서볼래?" component={WhereScreen} />
         </Tab.Navigator>
       </NavigationContainer>
     </>
@@ -38,10 +57,14 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
+  // button: {
+  //   padding:50,
+  //   backgroundColor: 'orange'
+  // }
 });
